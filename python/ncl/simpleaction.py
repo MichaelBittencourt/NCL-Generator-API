f7c523ffd942330ea8f409e97e4dbd5c2a562812
Michael Bittencourt <mchl.bittencourt@gmail.com>
2019-12-22 22:05:19 -0400

    chore: Change SimpleAction to use AbstractElement

--- python/ncl/simpleaction.py
+++ python/ncl/simpleaction.py
@@ -9,22 +9,35 @@
 """
 
 """
-from lxml import etree
+from ncl.abstractelement import AbstractElement
 
-class SimpleAction:
+class SimpleAction(AbstractElement):
 
-    role = None
+    def __init__(self, role, delay=None, eventType=None, actionType=None, value=None, min=None, max=None, qualifier=None, repeat=None, repeatDelay=None, duration=None, by=None):
+        listAttributes = ["role", "delay", "eventType", "actionType", "value", "min", "max", "qualifier", "repeat", "repeatDelay", "duration", "by"]
+        super().__init__("simpleAction", listAttributes, [])
+        self.set("role", role)
+        if delay is not None:
+            self.set("delay", delay)
+        if eventType is not None:
+            self.set("eventType", eventType)
+        if actionType is not None:
+            self.set("actionType", actionType)
+        if value is not None:
+            self.set("value", value)
+        if min is not None:
+            self.set("min", min)
+        if max is not None:
+            self.set("max", max)
+        if qualifier is not None:
+            self.set("qualifier", qualifier)
+        if repeat is not None:
+            self.set("repeat", repeat)
+        if repeatDelay is not None:
+            self.set("repeatDelay", repeatDelay)
+        if duration is not None:
+            self.set("duration", duration)
+        if by is not None:
+            self.set("by", by)
 
-    def __init__(self, role):
-        self.role = role
-
-    def getElement(self):
-        simpleAction = etree.Element("simpleAction")
-        simpleAction.set("role", self.role)
-        return simpleAction
-        
-
-    def generate(self):
-        print("Constructor call generate")
-
-    pass
+        pass
