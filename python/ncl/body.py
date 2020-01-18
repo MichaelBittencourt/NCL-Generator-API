11459e52a154986646cf67d8fd634012900aa894
Michael Bittencourt <mchl.bittencourt@gmail.com>
2020-01-17 23:05:28 -0400

    chore: Change body to use AbstractElement

--- python/ncl/body.py
+++ python/ncl/body.py
@@ -9,39 +9,24 @@
 """
 
 """
-from lxml import etree
+from ncl.abstractelement import AbstractElement
 from ncl.port import Port
+from ncl.property import Property
 from ncl.media import Media
 from ncl.link import Link
 
-class Body:
+class Body(AbstractElement):
 
-    listPort = []
-    listMedia = []
-    listLink = []
-
-    def __init__(self):
-        self.listPort = []
-        self.listMedia = []
-        self.listLink = []
-
-    def add(self, nclComponent):
-        if isinstance(nclComponent, Port):
-            self.listPort.append(nclComponent)
-        elif isinstance(nclComponent, Media):
-            self.listMedia.append(nclComponent)
-        elif isinstance(nclComponent, Link):
-            self.listLink.append(nclComponent)
-
-    def getElement(self):
-        head = etree.Element("body")
-        for i in self.listPort:
-            head.append(i.getElement())
-        for i in self.listMedia:
-            head.append(i.getElement())
-        for i in self.listLink:
-            head.append(i.getElement())
-        return head
+    def __init__(self, id=None):
+        listAttributes = ["id"]
+        """
+        TODO: Still needs add Context, Switch, Meta, Metadata
+        [OBS] The body can be a special Context
+        """
+        listChildren = [Port, Property, Media, Link]
+        super().__init__("body", listAttributes, listChildren)
+        if id is not None:
+            self.set("id", id)
 
     pass
 
